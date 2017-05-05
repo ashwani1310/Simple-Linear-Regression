be4e0630bdf58abaddfb2b879f4e9ead1076463e
ashwani1310 <ashwaniagarwal.1310@gmail.com>
2017-05-05 16:58:18 +0530

    Update simple_liner_regression_self.py

--- simple_liner_regression_self.py
+++ simple_liner_regression_self.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-data = pd.read_csv('Salary_Data.csv')
-Experience = data.iloc[:, :-1].values
-Salary = data.iloc[:, 1].values
+data = pd.read_csv('input here the destination of any csv file which has single input feature and a label')
+Single_feature = data.iloc[:, :-1].values #Here the label is last column and the feature is the first column
+Label = data.iloc[:, 1].values
 
-
+# Here the dataset is splitted into training and testing set. test_size=0.2 specifies that 20% of dataset will be testing data.
 from sklearn.cross_validation import train_test_split
-Exp_train, Exp_test, Sal_train, Sal_test = train_test_split(Experience, Salary, test_size = 0.2, random_state = 0)
+Feature_train, Feature_test, Label_train, Label_test = train_test_split(Single_feature, Label, test_size = 0.2, random_state = 0)
 
 
 # Simple linear regression model on training set
 from sklearn.linear_model import LinearRegression
 reg_model=LinearRegression()
-reg_model.fit(Exp_train, Sal_train)
+reg_model.fit(Feature_train, Label_train)
 
 #By fitting the linear regression model to the training set, we made our machine 
 #learn the correlation between the features and the label of training set.
@@ -23,19 +23,19 @@ reg_model.fit(Exp_train, Sal_train)
 #Now we use this model, which has learned correlations between the dependent 
 #and independent variables, to predict the test set results.
 
-Sal_predicted = reg_model.predict(Exp_test)
+Label_predicted = reg_model.predict(Feature_test)
 
 #Now, plotting the graphs
-plt.scatter(Exp_train, Sal_train, color = 'pink')
-plt.plot(Exp_train, reg_model.predict(Exp_train), color = 'green')
+plt.scatter(Feature_train, Label_train, color = 'pink')
+plt.plot(Feature_train, reg_model.predict(Feature_train), color = 'green')
 plt.title('Correlation in Training Set')
-plt.xlabel('Experience')
-plt.ylabel('Salary')
+plt.xlabel('Feature')
+plt.ylabel('Label')
 plt.show()
 
-plt.scatter(Exp_test, Sal_test, color = 'pink')
-plt.plot(Exp_train, regressor.predict(Exp_train), color = 'green')
+plt.scatter(Feature_test, Label_test, color = 'pink')
+plt.plot(Feature_train, regressor.predict(Feature_train), color = 'green')
 plt.title('Correlation in Test set')
-plt.xlabel('Experience')
-plt.ylabel('Salary')
+plt.xlabel('Feature')
+plt.ylabel('Label')
 plt.show()
